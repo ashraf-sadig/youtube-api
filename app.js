75176cf2a6f735383c42c8822c3dd2f874fb0b41
ashraf-sadig <ashraf.sadik@gmail.com>
2016-12-18 20:50:49 +0300

    added lightbox

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var API_BASE_URL = 'https://www.googleapis.com/youtube/v3/search';
+var apiKey = 'AIzaSyBMGbf47ZG8lownPukGjJpJxg6OXuc-EiI';
 
-function getDataFromApi(searchTerm, callback) {
-  var apiKey = '';
+function getDataFromApi(searchTerm, callback) {  
   var query = {
     part: 'snippet',
     key: apiKey,
@@ -10,26 +10,54 @@ function getDataFromApi(searchTerm, callback) {
   $.getJSON(API_BASE_URL, query, callback);
 }
 
+function getDataByChannelId(channelId) {
+	var query = {
+    	part: 'snippet',
+    	key: apiKey,
+    	channelId: channelId
+  	}
+  $.getJSON(API_BASE_URL, query, callback);
+}
 
-function displayOMDBSearchData(data) {
+function displaySearchData(data) {
   var resultElement = '';
+
   if (data.items) {
     data.items.forEach(function(item) {
-    resultElement += '<img class="thumbnails" src=' + item.snippet.thumbnails.medium.url + '>';
+    var channelId = item.snippet.channelId;
+    resultElement += '<a class="js-lightbox-video" href="https://www.youtube.com/watch?v=' + item.id.videoId + '">' + '<img class="thumbnails" src=' + item.snippet.thumbnails.medium.url + '>' + '</a><br>' +
+    '<button class="more-from-channel" data-channel-id‏="' + channelId + '">More from this channel</button>';
     });
   }
   else {
     resultElement += '<p>No results</p>';
   }
-  
   $('.js-search-results').html(resultElement);
+  bindSearchActions();
+}
+
+function bindSearchActions () {
+   var lightbox;
+  $('.js-lightbox-video').on('click', function(event) {
+    event.preventDefault();
+    var href = $(this).attr("href");
+    videoId = href.split("=")[1];
+    var lightbox = lity('//www.youtube.com/watch?v=' + videoId);
+  })
+
+  $('.more-from-channel').on('click', function(event) {
+    event.preventDefault();
+    var channelId = $(this).attr("data-channel-id‏");
+    getDataByChannelId(channelId);
+  })
+
 }
 
 function watchSubmit() {
   $('.js-search-form').submit(function(e) {
     e.preventDefault();
     var query = $(this).find('.js-query').val();
-    getDataFromApi(query, displayOMDBSearchData);
+    getDataFromApi(query, displaySearchData);
   });
 }
 
